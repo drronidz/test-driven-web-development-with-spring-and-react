303372eebc59aa02c6366fe38a4f01d66ca11c8f
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-04-27 17:18:21 +0200

    Setting FrontEnd project. closes #2.

---
+++ hoxify-frontend/src/App.js
@@ -0,0 +1,16 @@
+// Core React
+import React from "react";
+// Components
+// Styles
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
