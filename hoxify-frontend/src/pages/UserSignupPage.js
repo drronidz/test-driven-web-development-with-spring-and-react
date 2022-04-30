63519188e27c1ccda36645a8c187c156147cc0ae
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-04-30 17:40:14 +0200

    Handling the Input Change. closes #11.

--- hoxify-frontend/src/pages/UserSignupPage.js
+++ hoxify-frontend/src/pages/UserSignupPage.js
@@ -1,24 +1,78 @@
 import React from "react";
 
 export class UserSignUpPage extends React.Component{
+
+    state = {
+        displayName: '',
+        username: '',
+        password: '',
+        passwordConfirmation:''
+    }
+
+    onChangeDisplayName = (event) => {
+        const value = event.target.value
+        this.setState({
+            displayName : value
+        })
+    }
+
+    onChangeUsername = (event) => {
+        const value = event.target.value
+        this.setState({
+            username : value
+        })
+    }
+
+    onChangePassword = (event) => {
+        const value = event.target.value
+        this.setState({
+            password : value
+        })
+    }
+
+    onChangePasswordConfirmation = (event) => {
+        const value = event.target.value
+        this.setState({
+            passwordConfirmation : value
+        })
+    }
+
+
     render() {
         return (
             <div>
                 <h1>Sign Up</h1>
                 <div>
-                    <input placeholder="Your display name"/>
+                    <input
+                        type="text"
+                        placeholder="Your display name"
+                        value={this.state.displayName}
+                        onChange={this.onChangeDisplayName}
+                    />
                 </div>
                 <div>
-                    <input placeholder="Your username"/>
+                    <input
+                        type="text"
+                        placeholder="Your username"
+                        value={this.state.username}
+                        onChange={this.onChangeUsername}
+                    />
                 </div>
                 <div>
-                    <input placeholder="Your username"/>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={this.state.password}
+                        onChange={this.onChangePassword}
+                    />
                 </div>
                 <div>
-                    <input type="password" placeholder="Your password"/>
-                </div>
-                <div>
-                    <input type="password" placeholder="Your password confirmation"/>
+                    <input
+                        type="password"
+                        placeholder="Password confirmation"
+                        value={this.state.passwordConfirmation}
+                        onChange={this.onChangePasswordConfirmation}
+                    />
                 </div>
                 <div>
                     <button>Sign Up</button>
--- hoxify-frontend/src/pages/UserSignupPage.spec.js
+++ hoxify-frontend/src/pages/UserSignupPage.spec.js
@@ -3,6 +3,7 @@ import React from "react";
 import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import UserSignUpPage from "./UserSignupPage";
+import {fireEvent} from "@testing-library/dom";
 
 beforeEach(cleanup)
 
@@ -56,5 +57,48 @@ describe('UserSignUpPage', () => {
             const submitButton = container.querySelector('button')
             expect(submitButton).toBeInTheDocument()
         })
+    })
+    describe('Interactions', () => {
+
+        const changeEvent = (content) => {
+            return {
+                target: {
+                    value: content
+                }
+            }
+        }
+
+        it('sets the displayName value into state', () => {
+            const { queryByPlaceholderText } = render(<UserSignUpPage/>)
+            const displayNameInput  = queryByPlaceholderText('Your display name')
+
+            fireEvent.change(displayNameInput, changeEvent('my-display-name'))
+            expect(displayNameInput).toHaveValue('my-display-name')
+        })
+
+        it('sets the username value into state', () => {
+            const { queryByPlaceholderText } = render(<UserSignUpPage/>)
+            const usernameInput = queryByPlaceholderText('Your username')
+
+            fireEvent.change(usernameInput, changeEvent('my-user-name'))
+            expect(usernameInput).toHaveValue('my-user-name')
+        })
+
+        it('sets the password value into state', () => {
+            const { queryByPlaceholderText } = render(<UserSignUpPage/>)
+            const passwordInput = queryByPlaceholderText('Your password')
+
+            fireEvent.change(passwordInput, changeEvent('my-password'))
+            expect(passwordInput).toHaveValue('my-password')
+        })
+
+        it('sets password confirmation value into state', () => {
+            const { queryByPlaceholderText } = render(<UserSignUpPage/>)
+            const passwordConfirmationInput = queryByPlaceholderText('Password confirmation')
+
+            fireEvent.change(passwordConfirmationInput, changeEvent('my-password-confirmation'))
+            expect(passwordConfirmationInput).toHaveValue('my-password-confirmation')
+        })
+
     })
 })
