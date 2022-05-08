21ecfb18a13af1890e886402d6dae0b5115a6186
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-05-09 00:33:16 +0200

    Handling the Input Changes. closes #31

--- hoxify-frontend/src/pages/LoginPage/LoginPage.spec.js
+++ hoxify-frontend/src/pages/LoginPage/LoginPage.spec.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import  {LoginPage} from './LoginPage'
 
 describe('LoginPage', () => {
@@ -39,7 +39,31 @@ describe('LoginPage', () => {
         const button = container.querySelector('button')
         expect(button).toBeInTheDocument()
     })
+})
 
+describe('Interactions', () => {
+
+    const changeEvent = (content) => {
+        return {
+            target: {
+                value : content
+            }
+        }
+    }
+
+    it ('sets the username value into state', () => {
+        const { queryByPlaceholderText } = render(<LoginPage/>)
+        const usernameInput = queryByPlaceholderText('Your username')
+        fireEvent.change(usernameInput, changeEvent('my-user-name'))
+        expect(usernameInput).toHaveValue('my-user-name')
+    })
+
+    it ('sets the password value into state', () => {
+        const { queryByPlaceholderText } = render(<LoginPage/>)
+        const passwordInput = queryByPlaceholderText('Your password')
+        fireEvent.change(passwordInput, changeEvent('my-password'))
+        expect(passwordInput).toHaveValue('my-password')
+    })
 })
 
 
