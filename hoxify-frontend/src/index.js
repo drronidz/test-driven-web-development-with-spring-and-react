250f10f4d886f2e191af146a93e2d8f52b885894
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-05-14 16:31:40 +0200

    Setting initial Config for Redux. closes #43

--- hoxify-frontend/src/index.js
+++ hoxify-frontend/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './container/App';
 import {HashRouter} from "react-router-dom";
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import authReducer from "./redux/authReducer";
+
+const store = createStore(authReducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
