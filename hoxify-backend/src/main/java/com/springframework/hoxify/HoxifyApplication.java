a647d8745d3c062fee8314c91295999f658ef542
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-05-17 16:28:54 +0200

    Creating Random Users. closes #59

--- hoxify-backend/src/main/java/com/springframework/hoxify/HoxifyApplication.java
+++ hoxify-backend/src/main/java/com/springframework/hoxify/HoxifyApplication.java
@@ -1,8 +1,15 @@
 package com.springframework.hoxify;
 
+import com.springframework.hoxify.model.User;
+import com.springframework.hoxify.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class HoxifyApplication {
@@ -11,4 +18,18 @@ public class HoxifyApplication {
         SpringApplication.run(HoxifyApplication.class, args);
     }
 
+    @Bean
+    @Profile("test")
+    CommandLineRunner run(UserService userService) {
+        return (args -> {
+            IntStream.rangeClosed(1, 15)
+                    .mapToObj(i -> {
+                User user = new User();
+                user.setUsername("user" + i);
+                user.setDisplayName("display" + i);
+                user.setPassword("AZerty" + i + "" + (i+1));
+                return user;
+            }).forEach(userService::save);
+        });
+    }
 }
