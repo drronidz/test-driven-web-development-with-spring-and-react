cee594ec1c86d892199faa2f77a0c60630424041
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-05-17 13:41:47 +0200

    Modeling User Projection. closes #55

---
+++ hoxify-backend/src/main/java/com/springframework/hoxify/projection/UserProjection.java
@@ -0,0 +1,15 @@
+package com.springframework.hoxify.projection;
+
+/*
+PROJECT NAME : hoxify
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 5/17/2022 1:35 PM
+*/
+
+public interface UserProjection {
+    public long getId();
+    public String getUsername();
+    public String getDisplayName();
+    public String getImage();
+}
--- hoxify-backend/src/main/java/com/springframework/hoxify/repository/UserRepository.java
+++ hoxify-backend/src/main/java/com/springframework/hoxify/repository/UserRepository.java
@@ -8,9 +8,17 @@ DATE : 4/29/2022 4:13 PM
 */
 
 import com.springframework.hoxify.model.User;
+import com.springframework.hoxify.projection.UserProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
     User findByUsername(String username);
+
+    @Query(value = "SELECT * FROM USER", nativeQuery = true)
+    Page<UserProjection> getAllUsersProjection(Pageable pageable);
 }
